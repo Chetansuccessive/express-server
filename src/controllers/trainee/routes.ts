cca53dd2e8b00542bf41a195c0ccde8aadd7c72d
Chetansuccessive <chetan.mahajan@successive.tech>
2020-11-02 18:28:05 +0530

    fix

--- src/controllers/trainee/routes.ts
+++ src/controllers/trainee/routes.ts
@@ -1,12 +1,15 @@
-import { Router } from  'express';
-import  traineeController  from './Controller';
+import { Router } from 'express';
+
+import traineeController from './Controller';
+import validationHandler from "../../libs/validationHandler";
+import validation from './validation' ;
 
 const traineeRouter = Router();
 
 traineeRouter.route('/')
-          .get(traineeController.get)
-          .post(traineeController.create)
-          .put(traineeController.update)
-          .delete(traineeController.delete)
+    .get(validationHandler(validation.get),traineeController.get)
+    .post(traineeController.create)
+    .put(traineeController.update)
+    .delete(traineeController.delete);
 
-          export default traineeRouter;
+export default traineeRouter;
