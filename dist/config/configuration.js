ed3e4a61e7df976b5f08e3a9b227231c85ab67a8
Chetansuccessive <chetan.mahajan@successive.tech>
2020-11-05 12:50:17 +0530

    fix

--- dist/config/configuration.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv = require("dotenv");
-console.log('Inside config');
-console.log(dotenv);
-// const envVars= dotenv()
-const config = {};
-exports.default = config;
-//# sourceMappingURL=configuration.js.map
