b6c5461124c720191ddc81fc97cda184bceecf61
Chetansuccessive <chetan.mahajan@successive.tech>
2020-11-05 13:06:14 +0530

    fix

--- extraTs/utils/helpers.ts
+++
@@ -1,5 +0,0 @@
-export default function validateEmail(email) {
-    const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-    const domain = email.split('@')[1];
-    return reg.test(email) && (domain === 'successive.tech');
-}
