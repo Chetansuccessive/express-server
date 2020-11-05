47bf52be201303557ab5c1de69092cd5366ebbc3
Chetansuccessive <chetan.mahajan@successive.tech>
2020-11-05 13:01:17 +0530

    fix

--- dist/utils/helpers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function validateEmail(email) {
-    const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-    const domain = email.split('@')[1];
-    return reg.test(email) && (domain === 'successive.tech');
-}
-exports.default = validateEmail;
-//# sourceMappingURL=helpers.js.map
