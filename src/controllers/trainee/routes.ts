5296888c99feb13bfd169abeed9d8ab599b4a028
Chetansuccessive <chetan.mahajan@successive.tech>
2020-11-09 13:15:01 +0530

    fix

--- src/controllers/trainee/routes.ts
+++ src/controllers/trainee/routes.ts
@@ -8,10 +8,6 @@ import validationHandler from '../../libs/routes/validationHandler';
 const traineeRouter = Router();
 
 traineeRouter.route('/')
-.get(traineeController.get)
-.post(traineeController.create)
-.put(traineeController.update)
-.delete(traineeController.Delete)
 .get(validationHandler(validation.get), traineeController.get)
 .post(validationHandler(validation.create), traineeController.create)
 .put(validationHandler(validation.update), traineeController.update)
