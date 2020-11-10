6c36c8bb1d669e207cfbfc0e51139b005bca1801
Chetansuccessive <chetan.mahajan@successive.tech>
2020-11-10 11:16:05 +0530

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import Server from './Server';
-import { config } from './config';
-
+import { config }  from './config';
 console.log('Config is', config);
 
-const server = new Server({ PORT: 9000 });
+const server = new Server ({PORT: 9000});
 
+server.bootstrap().run();
 server.bootstrap().run();
